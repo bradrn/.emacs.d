84e99f225c3cd00a59f55ef027e9332db2efbd46
Brad Neimann <brad.neimann@hotmail.com>
2018-10-26 22:32:57 +1100

    Remove ugly fixed-pitch font

--- init.el
+++ init.el
@@ -751,4 +751,4 @@ the current frame."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t nil))))
