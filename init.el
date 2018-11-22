71fefc45f73543c1300eeb3d810104a72b270874
Brad Neimann <brad.neimann@hotmail.com>
2018-11-22 17:08:40 +1100

    Edit some annoying faces

--- init.el
+++ init.el
@@ -773,4 +773,5 @@ the current frame."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t nil))))
+ '(fixed-pitch ((t nil)))
+ '(preview-reference-face ((t (:height 1.5)))))
