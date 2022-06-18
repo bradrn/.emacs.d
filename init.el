6a06c6769ddf978eb74c1bf8f4787ca22db82727
Brad Neimann <brad.neimann@hotmail.com>
2022-06-18 16:49:59 +1000

    Use lispy and lispyville

--- init.el
+++ init.el
@@ -342,6 +342,37 @@ The optional argument NEW-WINDOW is not used."
 (use-package rainbow-delimiters
   :init
   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+(use-package lispy
+  :defer
+  :init
+  (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+  (add-hook 'lisp-mode-hook       (lambda () (lispy-mode 1)))
+  (add-hook 'lispy-mode-hook #'lispyville-mode)
+  :config
+  (setq lispy-close-quotes-at-end-p t))
+(use-package lispyville
+  :after (lispy yasnippet)
+  :commands lispyville-mode
+  :init
+  (with-eval-after-load 'evil
+    (let ((pos (memq 'evil-mode-line-tag mode-line-format)))
+      (setcdr pos (cons
+                   '(:eval (when (featurep 'lispyville)
+                             (lispyville-mode-line-string "l-special" "")))
+                   (cdr pos)))))
+  :config
+  (lispyville-set-key-theme
+   '(operators
+     c-w
+     text-objects
+     ;; atom-movement
+     additional-movement
+     commentary
+     slurp/barf-cp
+     wrap
+     additional
+     additional-insert
+     escape)))
 
 ;; flycheck
 (use-package flycheck
